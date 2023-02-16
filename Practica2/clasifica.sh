877822e7c5560203bd2a6162ae1a574712eadd3b
Ivanlpc <100932340+Ivanlpc@users.noreply.github.com>
2023-02-16 21:04:08 +0100

    clasifica fix

--- Practica2/clasifica.sh
+++ Practica2/clasifica.sh
@@ -5,7 +5,7 @@ if [ ! -d $2 ]
     fi
 for i in `ls $1`
 do
-    extension=$(echo $i | cut -d. -f2)
+    extension=$(echo $i | cut -d. -f2-)
     author=$(grep -E 'autor' $1/$i | cut -d: -f2)
     title=$(grep -E 'titulo' $1/$i | cut -d: -f2)
     if [ ! -d $2/$author ]
