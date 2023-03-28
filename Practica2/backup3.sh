7eba018e8975280070c314ab0966c43b64813e8c
Ivanlpc <100932340+Ivanlpc@users.noreply.github.com>
2023-03-28 18:44:28 +0200

    shell base

--- Practica2/backup3.sh
+++ Practica2/backup3.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+fecha=$(date +%g%m%d)
+if [ ! -d "backup" ]
+then
+    mkdir backup backup/$fecha
+fi
+
+if [ ! -d "backup/$fecha" ]
+then
+    mkdir backup/$fecha
+fi
+
 for i in "$@"
 do
     
@@ -6,15 +17,6 @@ do
     then
         echo "$i no existe"
     else
-        fecha=$(date +%g%m%d)
-        if [ ! -d "backup" ]
-        then
-            mkdir backup backup/$fecha
-        fi
-        if [ ! -d "backup/$fecha" ]
-        then
-            mkdir backup/$fecha
-        fi
         cp $i "./backup/$fecha/$i"
     fi
     
